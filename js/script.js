74fe1150230479c8b8bd08bfa055eecb6c7b761b
Andrejb596 <andrejb596@gmail.com>
2022-10-02 17:27:09 +0300

    адаптив готов

--- js/script.js
+++ js/script.js
@@ -4,16 +4,28 @@ $(function () {
         nextArrow: '<button class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1""/></svg></button>',
         infinite: false,
     });
-
+    
     $('.questions__item-title').on('click', function () {
         $('.questions__item').removeClass('questions__item--active');
         $(this).parent().addClass('questions__item--active');
     });
-
+    
     $('#fullpage').fullpage({
-		autoScrolling:true,
-		scrollHorizontally: true,
+        autoScrolling: true,
+        scrollHorizontally: true,
         sectionSelector: '.page-section',
-	});
+        menu: '#header__nav',
+        anchors: ['top','products', 'benefits', 'specification', 'questions', 'contacts'],
+    });
 
+    $('.menu__burger').on('click', function () {
+        $('.menu__burger').toggleClass('menu__burger--active');
+        $('.menu__list').toggleClass('menu__list--active');
+    })
+    
+    $('.menu__list-link').on('click', function () {
+        $('.menu__burger').removeClass('menu__burger--active');
+        $('.menu__list').removeClass('menu__list--active');
+    })
 });
+
